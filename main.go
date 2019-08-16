088c08f1f3516dc40abb4679ba630b2d92f93cb4
waspryo <wasp2123321@gmail.com>
2019-08-16 17:45:21 +0900

    Golang REST API With Mux

--- main.go
+++ main.go
@@ -51,19 +51,41 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 func createBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var book Book
-	_ = json.NewDecoder(r.body).Decode(&book)
+	_ = json.NewDecoder(r.Body).Decode(&book)
 	book.ID = strconv.Itoa(rand.Intn(10000000)) // mock ID - not safe
 	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
 }
 
 // Update book
 func updateBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[:index+1]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"]
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 // Delete book
 func deleteBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[:index+1]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 // Main function
